265312d9ac0599229ef837f24f2855cea3d5eacd
BAHRICHADHA <chathabahri793@gmail.com>
2022-10-05 15:22:56 +0200

    -LesGaulois-

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,20 +4,39 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetpotion = 1;
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
-	}
-	public int getEffetpotion() {
-		return effetpotion;
-	}
-	public void setEffetpotion(int effetpotion) {
-		this.effetpotion = effetpotion;
-	}
+	
+
 	public Gaulois(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	}
+
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le gaulois" + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + "envoie un grand coup dans le machoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+//	public String toString() {
+//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
+//	}
+
+	public static void main(String[] args) {
+		asterix (nom = "Astérix", force = 8);
+		System.out.println(asterix);
+
 	}
 
 }
